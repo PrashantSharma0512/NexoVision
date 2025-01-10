387fc4ddd73a7e4a68ba9cb00999dbd506501d3b
PrashantSharma0512 <prashantsharma0512@gmail.com>
2025-01-10 23:15:14 +0530

    update routes

--- Backend/routes/teacher.js
+++ Backend/routes/teacher.js
@@ -7,8 +7,12 @@ const teacherController = require('../controllers/teacherController');
 router.get('/', async(req,res)=>{
     res.send('teacher routes')
 })
+//get the details 
 router.get('/details',teacherController.Details)
+// Create a new teacher
 router.post('/create',teacherController.createTeacher)
+// Update a teacher
 router.put('/update/:id', teacherController.updateTeacher);
+// Delete a teacher
 router.delete('/delete/:id', teacherController.deleteTeacher);
 module.exports = router;
